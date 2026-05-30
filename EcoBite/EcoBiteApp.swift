1d77d4e364fd16510f5c5bb2bd75b3a18f979d70
pratama6624 <scyoung6624@gmail.com>
2026-05-30 11:24:57 +0700

    Initial Commit

---
+++ EcoBite/EcoBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoBiteApp.swift
+//  EcoBite
+//
+//  Created by Pratama One on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
